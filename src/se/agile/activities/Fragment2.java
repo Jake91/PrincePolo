59604c0f1b68b5901d540cc34a1e4d632084675b
Thor <thgautsson@gmail.com>
2014-03-31 21:35:39 +0200

    Put together a proper navigation menu for the app

---
+++ src/se/agile/activities/Fragment2.java
@@ -0,0 +1,22 @@
+package se.agile.activities;	
+
+import se.agile.princepolo.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+	
+	public Fragment2(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_2, container, false);
+         
+        return rootView;
+    }
+}
---
+++ src/se/agile/activities/ThorFragment.java
@@ -0,0 +1,22 @@
+package se.agile.activities;	
+
+import se.agile.princepolo.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ThorFragment extends Fragment {
+	
+	public ThorFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_thor, container, false);
+         
+        return rootView;
+    }
+}

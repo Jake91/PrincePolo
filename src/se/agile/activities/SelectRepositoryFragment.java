9bf37bb71df7d63491b71b649e7c278a9eb4d63e
jake <jlarsson_nr96@hotmail.com>
2014-04-04 18:47:33 +0200

    started on the select repository view

--- src/se/agile/activities/SelectRepositoryFragment.java
+++ src/se/agile/activities/SelectRepositoryFragment.java
@@ -1,22 +1,45 @@
 package se.agile.activities;	
 
+import se.agile.activities.model.Preferences;
 import se.agile.princepolo.R;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.webkit.WebView.FindListener;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SelectRepositoryFragment extends Fragment {
 	private String logTag;
+	private View rootView;
 	public SelectRepositoryFragment(){}
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 		logTag = getResources().getString(R.string.logtag_main);
-        View rootView = inflater.inflate(R.layout.fragment_select_repository, container, false);
-         
+        rootView = inflater.inflate(R.layout.fragment_select_repository, container, false);
         return rootView;
     }
+	
+	
+	@Override
+	public void onResume(){
+		RadioGroup group = (RadioGroup) rootView.findViewById(R.id.radiogroup_repos);
+		for(String repo : Preferences.getUserRepos()){
+			RadioButton button = new RadioButton(rootView.getContext());
+			button.setText(repo);
+			button.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
+			group.addView(button);
+		}
+		super.onResume();
+	}
+	/*
+layout = (LinearLayout) findViewById(R.id.statsviewlayout);
+
+	 */
 }
